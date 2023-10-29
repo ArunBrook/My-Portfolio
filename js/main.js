062ef036dbd54a110ed3c7512f25f6ac3d1eb2cf
arun <eaaunbrook@gmail.com>
2023-10-29 14:56:37 +0530

    overflow updated

--- js/main.js
+++ js/main.js
@@ -85,29 +85,35 @@
 
 	var carousel = function() {
 		$('.home-slider').owlCarousel({
-	    loop:true,
-	    autoplay: true,
-	    margin:0,
-	    animateOut: 'fadeOut',
-	    animateIn: 'fadeIn',
-	    nav:false,
-	    autoplayHoverPause: false,
-	    items: 1,
-	    navText : ["<span class='ion-md-arrow-back'></span>","<span class='ion-chevron-right'></span>"],
-	    responsive:{
-	      0:{
-	        items:1
-	      },
-	      600:{
-	        items:1
-	      },
-	      1000:{
-	        items:1
-	      }
-	    }
+			loop: true,
+			autoplay: true,
+			touchDrag: true,
+			margin: 0,
+			animateOut: 'fadeOut',
+			animateIn: 'fadeIn',
+			nav: false,
+			autoplayHoverPause: false,
+			items: 1,
+			navText: ["<span class='ion-md-arrow-back'></span>", "<span class='ion-chevron-right'></span>"],
+			 // Enable touch drag/swipe
+			responsive: {
+				0: {
+					items: 1
+				},
+				768: { // Adjust for mobile view
+					items: 1
+				},
+				1024: {
+					items: 1
+				}
+			}
 		});
 	};
+	
+	// Call the carousel function
 	carousel();
+	
+	
 
 	$('nav .dropdown').hover(function(){
 		var $this = $(this);
